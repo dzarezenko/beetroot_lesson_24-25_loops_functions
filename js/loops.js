b2d73a04d18f80fdc04b61e78c901c392a2756c7
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2019-10-30 19:40:49 +0200

    Initial commit (loops)

---
+++ js/loops.js
@@ -0,0 +1,20 @@
+//console.log("!!!");
+
+/*let i = 0;
+while (i < 10) {
+  console.log(i);
+  i++;
+}*/
+
+let i = 0;
+for (; i < 10; i++) {
+  console.log(i);
+}
+console.log(i);
+
+/*let i = 0;
+do {
+  console.log(i);
+  i++;
+} while (false);
+console.log(i);*/
