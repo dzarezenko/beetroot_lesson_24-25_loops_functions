f986778c087162c0583ded95275b6d16a29d21c1
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2019-10-30 19:48:12 +0200

    Infinite loop

--- js/loops.js
+++ js/loops.js
@@ -6,11 +6,11 @@ while (i < 10) {
   i++;
 }*/
 
-let i = 0;
+/*let i = 0;
 for (; i < 10; i++) {
   console.log(i);
 }
-console.log(i);
+console.log(i);*/
 
 /*let i = 0;
 do {
@@ -18,3 +18,20 @@ do {
   i++;
 } while (false);
 console.log(i);*/
+
+let i = 0;
+while (true) {
+
+  if (i == 5) {
+    i++;
+    continue;
+  }
+
+  if (i == 10) {
+    break;
+  }
+
+  console.log(i);
+
+  i++;
+}
