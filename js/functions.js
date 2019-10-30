64da272a0b8340dbc3f312384be397743c743755
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2019-10-30 20:25:12 +0200

    ...

--- js/functions.js
+++ js/functions.js
@@ -16,6 +16,13 @@ let sqr = (n) => {
 }
 //let a = 17;
 //let b = sqr(a);
-console.log(sqr(17));
+//console.log(sqr(17));
 
-console.log(typeof sqr);
+//console.log(typeof sqr);
+
+let power = (a, b, f) => {
+  return f(a, b);
+}
+
+let result = power(2, 4, (x, y) => x*x + y*y)
+console.log(result);
